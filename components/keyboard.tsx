a0524291c6713d2b4f1d7536c06d1ea66fd088ff
Timur <tiabumuaz@gmail.com>
2023-02-01 13:01:05 +0500

    Сверстан <Keyboard />

---
+++ components/keyboard.tsx
@@ -0,0 +1,137 @@
+import { useState } from 'react';
+import { keyboardLetters } from '@/const';
+import styles from '../styles/components/keyboard.module.scss';
+import cn from 'classnames';
+
+type KeyboardPropsType = {
+  onButtonClick: (letter: string) => void;
+  onClearButtonClick: () => void;
+  onSearchButtonClick: () => void;
+};
+
+function Keyboard({
+  onButtonClick,
+  onClearButtonClick,
+  onSearchButtonClick,
+}: KeyboardPropsType) {
+  const [isShiftPressed, setIsShiftPressed] = useState<boolean>(false);
+
+  const handleShiftButtonClick = () => {
+    setIsShiftPressed((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.row}>
+        {isShiftPressed
+          ? keyboardLetters.firstRow.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))
+          : keyboardLetters.firstRowB.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))}
+        <button
+          className={cn(styles.button, styles.clearButton)}
+          onClick={onClearButtonClick}
+        >
+          Очистить
+        </button>
+      </div>
+      <div className={styles.row}>
+        <button className={cn(styles.button, styles.tabButton)}></button>
+        {isShiftPressed
+          ? keyboardLetters.secondRow.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))
+          : keyboardLetters.secondRowB.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))}
+      </div>
+      <div className={styles.row}>
+        <button className={cn(styles.button, styles.capsButton)}></button>
+        {isShiftPressed
+          ? keyboardLetters.thirdRow.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))
+          : keyboardLetters.thirdRowB.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))}
+        <button
+          className={cn(styles.button, styles.searchButton)}
+          onClick={onSearchButtonClick}
+        >
+          Поиск
+        </button>
+      </div>
+      <div className={styles.row}>
+        <button
+          className={cn(styles.button, styles.leftShiftButton)}
+          onClick={handleShiftButtonClick}
+        >
+          Shift
+        </button>
+        {isShiftPressed
+          ? keyboardLetters.fourthRow.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))
+          : keyboardLetters.fourthRowB.map((letter, i) => (
+              <button
+                className={styles.button}
+                key={letter + i}
+                onClick={() => onButtonClick(letter)}
+              >
+                {letter}
+              </button>
+            ))}
+        <button
+          className={cn(styles.button, styles.rightShiftButton)}
+          onClick={onSearchButtonClick}
+        ></button>
+      </div>
+    </div>
+  );
+}
+
+export default Keyboard;
--- components/search.tsx
+++ components/search.tsx
@@ -1,28 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
+import Keyboard from './keyboard';
 import searchButtonIcon from '../public/images/search-button-icon.svg';
 import keyboardIcon from '../public/images/keyboard-icon.svg';
 import styles from '../styles/components/search.module.scss';
 
 function Search({ query }: any) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [inputValue, setInputValue] = useState<string>('');
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState<boolean>(false);
 
   const router = useRouter();
 
-  useEffect(() => {
-    setInputValue(router.query.searchQuery as string);
-  }, []);
+  // useEffect(() => {
+  //   setInputValue(router.query.searchQuery as string);
+  // }, []);
 
   const handleInputChange = (value: string) => {
     setInputValue(value);
   };
 
-  const handleButtonClick = () => {
+  const handleSearchButtonClick = () => {
     inputValue
       ? router.push(`/words?searchQuery=${inputValue}`)
       : router.push(`/words`);
+    setIsKeyboardOpen(false);
+  };
+
+  const handleKeyboardIconClick = () => {
+    setIsKeyboardOpen((prev) => !prev);
+  };
+
+  const handleKeyboardButtonClick = (letter: string) => {
+    setInputValue((prev) => prev + letter);
+    inputRef.current?.focus();
+  };
+
+  const handleClearButtonClick = () => {
+    setInputValue('');
+    inputRef.current?.focus();
   };
 
   return (
@@ -30,10 +48,12 @@ function Search({ query }: any) {
       <div className={styles.inputContainer}>
         <input
           defaultValue={query}
+          value={inputValue}
           className={styles.input}
           type="text"
           placeholder="Введите слово"
           onChange={(evt) => handleInputChange(evt.target.value)}
+          ref={inputRef}
         />
         <Image
           className={styles.keyboardIcon}
@@ -41,11 +61,19 @@ function Search({ query }: any) {
           alt="Иконка клавиатуры"
           width={40}
           height={24}
+          onClick={handleKeyboardIconClick}
         />
+        {isKeyboardOpen && (
+          <Keyboard
+            onButtonClick={handleKeyboardButtonClick}
+            onClearButtonClick={handleClearButtonClick}
+            onSearchButtonClick={handleSearchButtonClick}
+          />
+        )}
         <button
           className={styles.button}
           type="button"
-          onClick={handleButtonClick}
+          onClick={handleSearchButtonClick}
         >
           <Image
             className={styles.buttonIcon}
